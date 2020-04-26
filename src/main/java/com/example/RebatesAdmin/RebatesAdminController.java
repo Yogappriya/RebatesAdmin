fda06d18f3b0ff67648a213ef16bb3bacc67a94a
Yogappriya Madhavan <yogappriya_madhavan@comcast.com>
2020-04-26 15:47:47 -0400

    added welcome msg

--- src/main/java/com/example/RebatesAdmin/RebatesAdminController.java
+++ src/main/java/com/example/RebatesAdmin/RebatesAdminController.java
@@ -1,4 +1,13 @@
 package com.example.RebatesAdmin;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RebatesAdminController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome!";
+    }
 }
